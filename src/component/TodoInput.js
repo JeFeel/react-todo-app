9450f1d676d07eb1068b1f8437751b0b3e60c5c1
JeFeel <jaephil94@gmail.com>
2023-05-24 12:07:27 +0900

    react todo project list check

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {MdAdd} from "react-icons/md";
+import cn from 'classnames';
+
+import './scss/TodoInput.scss'
+
 
 const TodoInput = () => {
+    //입력창 open 여부를 표현하는 상태값
+    const [open, setOpen] = useState(false);
+
+    // + 버튼 클릭시 이벤트 처리
+    const onToggle = () => {
+        setOpen(!open); //open값을 뒤집어줌
+    };
+    // const showForm = () => {
+    //         return }
+
     return (
-        <div>
-           TodoInput
-        </div>
+        <>
+            {
+                open && (<div className={'form-wrapper'}>
+                    <form className={'insert-form'}>
+                        <input
+                            type={'text'}
+                            placeholder={'할 일을 입력한 후 엔터를 누르세요'}
+                        />
+                    </form>
+                </div>)
+            }
+            {/* cn() 첫번째 파라미터는 항상 유지할 클래스, 두 번째 파라미터는 논리 상태값
+                => 논리상태값이 true일 경우 해당 클래스 추가, false의 경우 제거*/}
+            <button className={cn('insert-btn', {open})} onClick={onToggle}>
+                <MdAdd/>
+            </button>
+
+        </>
     );
 };
 
--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {MdDelete, MdDone} from "react-icons/md";
+import cn from 'classnames';
+import './scss/TodoItem.scss';
 
-const TodoItem = () => {
+const TodoItem = ({item}) => {
+
+    const {id, title, done} = item;
+
     return (
-        <li className={'todo-list-item'}>TodoItem</li>
+        <li className={'todo-list-item'}>
+            <div className={cn('check-circle', {active: done})}>
+                {done && <MdDone />}
+            </div>
+            <span className={cn('text', {finish: done})}>{title}</span>
+            <div className={'remove'}>
+                <MdDelete />
+            </div>
+        </li>
     );
 };
 
