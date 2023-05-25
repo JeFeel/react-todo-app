ea4f05293de382494b4888334b16c8746aa7cea6
JeFeel <jaephil94@gmail.com>
2023-05-25 09:40:41 +0900

    react todo project input script

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -9,6 +9,9 @@ const TodoInput = () => {
     //입력창 open 여부를 표현하는 상태값
     const [open, setOpen] = useState(false);
 
+    // 할일 입력창에 입력한 내용을 표현하는
+    const [todoText, setTodoText] = useState('');
+
     // + 버튼 클릭시 이벤트 처리
     const onToggle = () => {
         setOpen(!open); //open값을 뒤집어줌
@@ -16,14 +19,35 @@ const TodoInput = () => {
     // const showForm = () => {
     //         return }
 
+    //submit change handler
+    const submitHandler = e =>{
+        e.preventDefault(); //submit의 페이지 넘어가는 기능 제한
+        // console.log('폼 제출');
+        console.log(todoText);
+
+        // 입력이 끝나면 입력창 비우기
+        setTodoText('');
+        //value={todoText}가 있어야만 '' 랜더링까지 된다
+    };
+
+    //input change handler
+    const todoChangeHandler = e => {
+        // console.log(e.target);
+        setTodoText(e.target.value);
+    };
+
+
     return (
         <>
             {
                 open && (<div className={'form-wrapper'}>
-                    <form className={'insert-form'}>
+                    <form className={'insert-form'} onSubmit={submitHandler}>
                         <input
                             type={'text'}
                             placeholder={'할 일을 입력한 후 엔터를 누르세요'}
+                            onChange={todoChangeHandler}
+                            autoFocus
+                            value={todoText}
                         />
                     </form>
                 </div>)
